01b0cec236118a681cc0bae1303f0b4284455589
msamancioglu <msamancioglu@hotmail.com>
2019-04-01 12:04:45 +0300

    I noticed I had misspelled the word recently at the end of the 2nd line

--- first_R_testing.R
+++ first_R_testing.R
@@ -1,2 +1,2 @@
 print("This file was created within RStudio")
-print("And now it lives on GitHub -- This line has been added recenyly")
+print("And now it lives on GitHub -- This line has been added recenylty")
