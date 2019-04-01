46218934908723b5398fc3d75fb43c787ed53718
msamancioglu <msamancioglu@hotmail.com>
2019-04-01 15:02:07 +0300

    -- please explain this section :) ms

--- first_R_testing.R
+++ first_R_testing.R
@@ -1,3 +1,4 @@
 print("This file was created within RStudio")
 print("And now it lives on GitHub -- This line has been added recenylty")
 print("added this from testing2 project")
+-- please explain this section :) ms
